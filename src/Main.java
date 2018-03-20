19f4c30cd7f24e224eef0e1a89a40a950330ebf9
Alleluid <Milotig@gmail.com>
2018-03-19 20:59:05 -0700

    Added some object stuff

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,20 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+class Contact{
+    String name;
+    String email;
+    String phonenumber;
+}
+
+class ContactsManager {
+    // Fields:
+    Contact [] myFriends;
+    int friendsCount;
+    //Constructor:
+    ContactsManager(){
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+}
